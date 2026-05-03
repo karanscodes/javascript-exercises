61d75daaeaea0738be097853285fcbbf351193c0
karanscodes <karanadityasingh60@gmail.com>
2026-05-03 12:57:39 +0530

    Test cases passed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = '';
 
+    let i = string.length - 1; 
+    while(i >= 0){
+        newString += string[i--];
+    }
+    return newString;
 };
 
 // Do not edit below this line
