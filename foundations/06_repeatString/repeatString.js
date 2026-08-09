ad16e1f407e4dca629e2c8b67c099d1f528f0977
karanscodes <karanadityasingh60@gmail.com>
2026-08-09 14:44:21 +0530

    8/8 passed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    if(num < 0) return "ERROR";
+    if(num == 0) return "";
+    let str = "";
+    for(let i = 0; i < num; i++){
+        str += s;
+    }
+    return str;
 };
 
 // Do not edit below this line
